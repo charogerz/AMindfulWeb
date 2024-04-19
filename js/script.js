968af15ccce81b789fb8b89ba40f29524b29ae98
rogec540 <charogerz@gmail.com>
2024-04-18 22:59:08 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+if (document.getElementById('examples-link')) {
+  document.getElementById('examples-link').addEventListener('click', () => {
+    document.getElementById('examples-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if (document.getElementById('more-info-link')) {
+  document.getElementById('more-info-link').addEventListener('click', () => {
+    document.getElementById('more-info-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
+if (document.getElementById('resources-link')) {
+  document.getElementById('resources-link').addEventListener('click', () => {
+    document.getElementById('resources-section').scrollIntoView({behavior: "smooth"})
+  })
+}
