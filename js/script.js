364987b88f02fb34043ec8f93d7c81b5b43370da
rogec540 <112709026+charogerz@users.noreply.github.com>
2024-05-01 20:16:32 -0400

    adding links to navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// jumplink function to about project section
+if (document.getElementById('about-link')) {
+  document.getElementById('about-link').addEventListener('click', () => {
+    document.getElementById('about-section').scrollIntoView({ behavior: "smooth" })
+  })
+}
+
 // jumplink function to example videos section
 if (document.getElementById('examples-link')) {
   document.getElementById('examples-link').addEventListener('click', () => {
